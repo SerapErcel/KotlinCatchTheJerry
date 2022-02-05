19fe7d9ca9cf1b44e5e1c79d5014361ad4ef7b00
sercel23 <ercelserap@gmail.com>
2022-02-05 23:06:16 +0300

    Add timer

--- app/src/main/java/com/example/kotlincatchthejerry/MainActivity.kt
+++ app/src/main/java/com/example/kotlincatchthejerry/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.kotlincatchthejerry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.kotlincatchthejerry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //CountDown Timer
+        object : CountDownTimer(15000, 1000) {
+            override fun onFinish() {
+                binding.timeText.text = "Time: 0"
+
+                //Alert
+                val alert = AlertDialog.Builder(this@MainActivity)
+                alert.setTitle("Game Over")
+                alert.setMessage("Restart The Game?")
+                alert.setPositiveButton("Yes") { dialog, which ->
+                    //Restart
+                    val intent = intent
+                    finish()
+                    startActivity(intent)
+                }
+                alert.setNegativeButton("No") { dialog, which ->
+                    Toast.makeText(this@MainActivity, "Game Over", Toast.LENGTH_LONG).show()
+                }
+                alert.show()
+
+            }
+
+            override fun onTick(p0: Long) {
+                binding.timeText.text = "Time: " + p0 / 1000
+            }
+
+
+        }.start()
     }
 
     fun increaseScore(view: View) {
